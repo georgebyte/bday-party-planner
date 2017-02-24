3e0ae90a3f98baf41d7c05a1b304fe3559f60275
Luka Šilovinac <luka.silovinac@zemanta.com>
2017-02-24 16:47:18 +0100

    Fix user roles representation

--- server/planner/serializers.py
+++ server/planner/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from . import models
+from . import constants
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -21,19 +22,48 @@ class UserUpdateSerializer(serializers.ModelSerializer):
 
 class UserRoleSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+    role = serializers.SerializerMethodField()
 
     class Meta(object):
         model = models.UserRole
         fields = ('user', 'role')
 
+    def get_role(self, user_role):
+        return {
+            'title': constants.USER_ROLE_TITLE[user_role.role],
+            'description': constants.USER_ROLE_DESCRIPTION[user_role.role],
+        }
+
 
 class PartySerializer(serializers.ModelSerializer):
-    users = UserRoleSerializer(many=True, source='userrole_set')
+    organizers = serializers.SerializerMethodField()
+    gift_buyers = serializers.SerializerMethodField()
 
     class Meta(object):
         model = models.Party
-        fields = ('id', 'date', 'deadline', 'users')
+        fields = ('id', 'date', 'deadline', 'organizers', 'gift_buyers')
 
+    def get_organizers(self, party):
+        organizers = party.userrole_set.filter(role=constants.UserRoleType.ORGANIZER)
+        serializer = UserRoleSerializer(many=True, instance=organizers)
+        return serializer.data
+
+    def get_gift_buyers(self, party):
+        organizers = party.userrole_set.exclude(role=constants.UserRoleType.ORGANIZER)
+        serializer = UserRoleSerializer(many=True, instance=organizers)
+        return serializer.data
+
+    def to_representation(self, obj):
+        ret = super(PartySerializer, self).to_representation(obj)
+        for organizer in ret['organizers']:
+            organizer.update(organizer['user'])
+            del organizer['user']
+
+        for gift_buyer in ret['gift_buyers']:
+            gift_buyer.update(gift_buyer['user'])
+            del gift_buyer['user']
+
+        return ret
 
 class GiftIdeaCommentSerializer(serializers.ModelSerializer):
     created_by = UserSerializer()
