5fbba05a0df0a2dab0fa8512f9d3c4e6904f7eba
Luka Šilovinac <luka.silovinac@zemanta.com>
2017-02-24 13:26:00 +0100

    Add is_staff field to user

--- server/planner/models.py
+++ server/planner/models.py
@@ -10,7 +10,7 @@ from . import constants
 
 class UserManager(BaseUserManager):
 
-    def _create_user(self, email, password, is_superuser, **extra_fields):
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
         """
         Creates and saves a User with the given email and password.
         """
@@ -19,6 +19,7 @@ class UserManager(BaseUserManager):
         now = timezone.now()
         user = self.model(
             email=email,
+            is_staff=is_staff,
             is_superuser=is_superuser,
             last_login=now,
             **extra_fields
@@ -28,20 +29,30 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        return self._create_user(email, password, False, **extra_fields)
+        return self._create_user(email, password, False, False, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
-        return self._create_user(email, password, True, **extra_fields)
+        return self._create_user(email, password, False, True, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     birthday = models.DateField()
     email = models.EmailField(_('email address'), max_length=255, unique=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.')
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['birthday']
 
     objects = UserManager()
+
+    def get_short_name(self):
+        "Returns the short name for the user."
+        return self.email
 
 
 class Party(models.Model):
