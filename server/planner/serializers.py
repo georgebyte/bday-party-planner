73a6314800e5bc47384833c294972126271b94ce
Luka Šilovinac <luka.silovinac@zemanta.com>
2017-02-24 15:25:01 +0100

    Add user role serializer

--- server/planner/serializers.py
+++ server/planner/serializers.py
@@ -10,7 +10,16 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'email', 'birthday')
 
 
+class UserRoleSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta(object):
+        model = models.UserRole
+        fields = ('user', 'role')
+
+
 class PartySerializer(serializers.ModelSerializer):
+    users = UserRoleSerializer(many=True, source='userrole_set')
 
     class Meta(object):
         model = models.Party
