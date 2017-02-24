ccaca3cce3b01ed311b0f704c36c927d87325d70
Jure Bajt <jure.bajt@gmail.com>
2017-02-24 11:36:16 +0100

    Added navigation links and basic layout.

--- client/src/app/components/components.module.ts
+++ client/src/app/components/components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainNavigationComponent } from './main-navigation/main-navigation.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
